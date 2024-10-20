c7efffe9299b327ce451b7d2fcdab775976fd93c
Joel-Palomino <U19218695@utp.edu.pe>
2024-10-19 20:04:05 -0500

    Agregando servicio de cache

--- microservices/search-service/search-service/src/main/java/com/utpconnectplatform/search_service/SearchServiceApplication.java
+++ microservices/search-service/search-service/src/main/java/com/utpconnectplatform/search_service/SearchServiceApplication.java
@@ -2,8 +2,12 @@ package com.utpconnectplatform.search_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
+@EnableFeignClients
 public class SearchServiceApplication {
 
 	public static void main(String[] args) {
--- microservices/search-service/search-service/src/main/java/com/utpconnectplatform/search_service/service/SearchService.java
+++ microservices/search-service/search-service/src/main/java/com/utpconnectplatform/search_service/service/SearchService.java
@@ -1,17 +1,15 @@
 package com.utpconnectplatform.search_service.service;
 
-import com.utpconnectplatform.search_service.client.UserClient;
 import com.utpconnectplatform.search_service.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 @Service
 public class SearchService {
     @Autowired
-    private UserClient userClient;
+    private SearchCacheService userCacheService;
 
     public User searchUser(Long id) {
-        return userClient.getUserById(id);
+        return userCacheService.getUserById(id);
     }
 }
