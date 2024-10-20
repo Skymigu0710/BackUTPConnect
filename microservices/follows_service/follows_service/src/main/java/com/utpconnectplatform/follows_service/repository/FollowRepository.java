a9ca9337bafc5a0c0083227333c5cf95a51561ba
crisNATALY <U22103883@utp.edu.pe>
2024-10-19 20:27:39 -0500

    Eliminando carpetas erroneas

--- microservices/follows_service/follows_service/src/main/java/com/utpconnectplatform/follows_service/repository/FollowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utpconnectplatform.follows_service.repository;
-
-import com.utpconnectplatform.follows_service.model.Follows;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FollowRepository extends JpaRepository <Follows, Long> {
-}
